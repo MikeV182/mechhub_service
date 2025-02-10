6a14be2827b4f7042355ebbee823af8100e66b22
Mike Vays <michailvays@gmail.com>
2025-02-10 22:44:03 +0700

    Deleted useless generated content, added navbar component

---
+++ mechhub_app/src/Components/NavBar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import logo from '../Assets/logo.svg'
+import '../Styles/NavBar.css'
+
+export default function NavBar() {
+  return (
+    <nav className='container'>
+        <img src={logo} alt="" className='logo' />
+        <ul>
+            <li>О факультете</li>
+            <li>Хакатоны</li>
+        </ul>
+    </nav>
+  );
+}
